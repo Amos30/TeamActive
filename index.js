81fae053625af7788e208e3f4d3711e7d49dd811
Amos30 <amos@afewdevelopers.com>
2024-06-12 04:28:37 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+function csvDataToText(csv) {
+    $.ajax({
+        url: csv,
+        type: 'get',
+        async: false,
+        success: function(csvText) {
+            csvTextToArray(csvText)
+        }
+    })
+}
+
+function csvTextToArray(csvText) {
+    Papa.parse(csvText, {
+        complete: function(results) {
+            csvArrayToTable(results.data)
+        }
+    })
+}
+
+function csvArrayToTable(csvArray) {
+    console.log("Finished:", csvArray)
+    for (let i in csvArray) {
+        if (i == 0) {
+            $('#stats-table-header-id').html(csvArray[i][0])
+            continue
+        } else if (i == 1) {
+            $('#stats-table-id').append('<tr class="active-row"><td>' + csvArray[i][0] + '</td><td>' + csvArray[i][1] + '</td></tr>')
+            continue
+        }
+
+        $('#stats-table-id').append('<tr><td>' + csvArray[i][0] + '</td><td>' + csvArray[i][1] + '</td></tr>')
+    }
+}
