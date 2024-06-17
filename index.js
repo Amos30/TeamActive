fb2f30346263a1242695ded0a4b527ea5a9f3ea9
Amos30 <amos@afewdevelopers.com>
2024-06-16 21:41:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,25 +18,55 @@ function csvTextToArray(csvText) {
 }
 
 function csvArrayToTable(csvArray) {
-    for (let i in csvArray) {
-        var name = csvArray[i][0]
-        var firstScore = csvArray[i][1]
-        var secondScore = csvArray[i][2]
-        var difference = secondScore - firstScore
+    var tableArray = createTableData(csvArray)
 
-        if (!name || name.trim().length == 0) {
+    for (let i in tableArray) {
+        var rowData = tableArray[i]
+        var difference = rowData.secondScore - rowData.firstScore
+
+        if (!rowData.name || rowData.name.trim().length == 0) {
             continue
         }
 
         if (i == 0) {
             // title
-            $('#stats-table-header-id').html(name)
+            $('#stats-table-header-id').html(rowData.name)
         } else if (i == 1) {
             // column labels
-            $('#stats-table-id').append('<tr class="active-row"><td>' + name + '</td><td>' + firstScore + '</td><td>' + secondScore + '</td><td>Increase</td></tr>')
+            $('#stats-table-id').append('<tr class="active-row"><td>' + rowData.name + '</td><td>' + rowData.firstScore + '</td><td>' + rowData.secondScore + '</td><td>Increase</td></tr>')
         } else {
             // column data
-            $('#stats-table-id').append('<tr><td><b>' + name + '</b></td><td>' + firstScore + '</td><td>' + secondScore + '</td><td>' + difference + '</td></tr>')
+            $('#stats-table-id').append('<tr><td><b>' + rowData.name + '</b></td><td>' + rowData.firstScore + '</td><td>' + rowData.secondScore + '</td><td>' + difference + '</td></tr>')
         }
     }
+}
+
+function createTableData(csvArray) {
+    var array = []
+    var headers = []
+    for (let i in csvArray) {
+        var rowData = {}
+
+        rowData.name = csvArray[i][0]
+        rowData.firstScore = csvArray[i][1]
+        rowData.secondScore = csvArray[i][2]
+
+        if (i == 0 || i == 1) {
+            headers.push(rowData)
+            continue
+        }
+
+        array.push(rowData)
+    }
+    
+    array.sort(compareIncrease)
+    array.splice(0, 0, ...headers)
+
+    return array
+}
+
+function compareIncrease(a, b) {
+    var firstDifference = a.secondScore - a.firstScore
+    var secondDifference = b.secondScore - b.firstScore
+    return secondDifference > firstDifference
 }
