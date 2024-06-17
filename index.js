d51a56da5ad4c83c2b2d96407dfc541bcfc0da38
Amos30 <amos@afewdevelopers.com>
2024-06-16 20:42:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,14 +19,23 @@ function csvTextToArray(csvText) {
 
 function csvArrayToTable(csvArray) {
     for (let i in csvArray) {
+        var name = csvArray[i][0]
+        var firstScore = csvArray[i][1]
+        var secondScore = csvArray[i][2]
+        var difference = secondScore - firstScore
+
         if (i == 0) {
-            $('#stats-table-header-id').html(csvArray[i][0])
+            // title
+            $('#stats-table-header-id').html(name)
             continue
         } else if (i == 1) {
-            $('#stats-table-id').append('<tr class="active-row"><td>' + csvArray[i][0] + '</td><td>' + csvArray[i][1] + '</td></tr>')
+
+            // column labels
+            $('#stats-table-id').append('<tr class="active-row"><td>' + name + '</td><td>' + firstScore + '</td><td>' + secondScore + '</td><td>Increase</td></tr>')
             continue
         }
 
-        $('#stats-table-id').append('<tr><td>' + csvArray[i][0] + '</td><td>' + csvArray[i][1] + '</td></tr>')
+        // column data
+        $('#stats-table-id').append('<tr><td>' + name + '</td><td>' + firstScore + '</td><td>' + secondScore + '</td><td>' + difference + '</td></tr>')
     }
 }
